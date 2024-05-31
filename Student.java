afdfc7c8fd7e80a57d16698ac68f885a8a43e84a
sravya <sraavya01@gmail.com>
2024-05-31 12:01:33 +0530

    student admisson form

---
+++ Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    public String name;
+    public int adNo;
+    public char gender;
+    public String dob;
+    public String mobNo;
+}
---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.adNo = 1;
+        s1.dob = "25/08/2000";
+        s1.gender = 'f';
+        s1.mobNo = "9701184620";
+        s1.name = "sravya";
+
+        Student s2 = new Student();
+        s2.adNo = 2;
+        s2.dob = "21/10/2003";
+        s2.gender = 'f';
+        s2.mobNo = "9059017317";
+        s2.name = "meghana";
+    }
+}
